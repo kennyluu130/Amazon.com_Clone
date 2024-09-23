f4bc540102219653fdbc9d0b52c1d376d91d0006
kennyluuuuuuu <kennyluu130@gmail.com>
2024-09-22 20:24:26 -0700

    Fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
@@ -30,7 +31,7 @@ cart.forEach((cartItem) => {
             ${matchingProduct.name}
           </div>
           <div class="product-price">
-            $${matchingProduct.priceCents / 100}
+            $${formatCurrency(matchingProduct.priceCents)}
           </div>
           <div class="product-quantity">
             <span>
