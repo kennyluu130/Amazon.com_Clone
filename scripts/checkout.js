8b3a3a5fcba075783030d7d88b9c5661db4466ba
kennyluuuuuuu <kennyluu130@gmail.com>
2024-09-22 20:37:51 -0700

    delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -103,5 +103,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
