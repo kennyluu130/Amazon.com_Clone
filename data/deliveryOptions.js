bc92fbfa714e7124c8719292b72b38929e92295e
kennyluuuuuuu <kennyluu130@gmail.com>
2024-09-23 16:49:18 -0700

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: "1",
+    deliveryDays: 7,
+    priceCents: 0,
+  },
+  {
+    id: "1",
+    deliveryDays: 3,
+    priceCents: 499,
+  },
+  {
+    id: "1",
+    deliveryDays: 1,
+    priceCents: 999,
+  },
+];
